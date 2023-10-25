7c8dbab73dadebc675c5f0e20c1093310653cb45
Antony lampidakis <121696497+antonylampidakis@users.noreply.github.com>
2023-10-25 22:39:18 +0300

    Add files via upload

---
+++ c programms/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double a,b,c,d;
+
+    printf("Dose platos:\n");
+    scanf("%lf", &a);
+
+    printf("Dose mikos:\n");
+    scanf("%lf", &b);
+
+    c=a*b;
+    d=(2*a)+(2*b);
+
+    printf("Emvadon: %lf\n", c);
+    printf("Perimetros: %lf\n", d);
+
+    return 0;
+}
