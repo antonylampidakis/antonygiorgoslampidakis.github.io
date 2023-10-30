6ef4248c4ee587bf32d519a7461f83b22750f13b
Antony lampidakis <121696497+antonylampidakis@users.noreply.github.com>
2023-10-30 14:41:24 +0200

    Add files via upload

---
+++ c programms/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void message() {
+  printf(" Antony \n");
+  printf("Lampidakis\n");
+  printf("ekpa \n");
+  printf("tmima pliroforikis kai tilepikinonion\n");
+
+}
+
+int main() {
+
+  message();
+  return 0;
+}
