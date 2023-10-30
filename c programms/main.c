73c72e3361aba81b8cae9f14ebb4f68342e5e763
Antony lampidakis <121696497+antonylampidakis@users.noreply.github.com>
2023-10-30 13:47:11 +0200

    Add files via upload

---
+++ c programms/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int A[10];
+
+   int max=A[0];
+   int c=0;
+
+   for (int i=0; i<=10; i++)
+   {
+       printf("Dose stoixio : \n");
+       scanf("%d",&A[i]);
+   }
+
+
+   for (int i=1; i<=10; i++)
+   {
+
+       if ( A[i]>=max) {    max=A[i];
+                            c=i;        }
+
+
+   }
+
+
+    printf("o megistos arithmos einai: %d \n", max);
+    printf("o megistos vrethike stin thesi: %d \n", c+1);
+
+    return 0;
+}
