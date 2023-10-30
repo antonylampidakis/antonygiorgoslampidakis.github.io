36ac0e157c26e4c837a468565bd14f9f00bbec6a
Antony lampidakis <121696497+antonylampidakis@users.noreply.github.com>
2023-10-30 14:47:32 +0200

    Add files via upload

---
+++ c programms/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void input (){
+
+    int A[10];
+
+    for (int i=0; i<10; i++){
+        printf("Dose timi ston pinaka : \n");
+        scanf("%d",&A[i]);
+    }
+}
+
+void output (){
+
+    int A[10];
+
+    for (int i=0; i<10; i++){
+        printf("Ta stoixia toy pinaka einai : %d\n",A[i]);
+    }
+}
+
+void mo (){
+    int A[10];
+    int sum=0;
+    double mo;
+
+    for(int i=0; i<10; i++){
+        sum=sum+A[i];
+    }
+    mo=sum/10;
+    printf("O mesos oros ton stixion einai : %lf\n",mo);
+}
+
+int main()
+{
+    input();
+    output();
+    mo();
+    return 0;
+}
